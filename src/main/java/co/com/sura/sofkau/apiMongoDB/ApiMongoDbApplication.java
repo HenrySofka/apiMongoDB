ed6b8879360f8ec0fbc34e9661014af5b6a1a532
HenrySofka <henry.ferreira@sofka.com.co>
2022-06-13 15:43:32 -0300

    first commit

---
+++ src/main/java/co/com/sura/sofkau/apiMongoDB/ApiMongoDbApplication.java
@@ -0,0 +1,13 @@
+package co.com.sura.sofkau.apiMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/sura/sofkau/apiMongoDB/ApiMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sura.sofkau.apiMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
